8867488544401d76c65040ed8f1ce5e0bd5a1fcb
Nathaniel Moschkin <ironywrit@gmail.com>
2020-12-10 16:00:47 -0500

    Update MACADDRESS.cs

--- WizLib/Wiz/MACADDRESS.cs
+++ WizLib/Wiz/MACADDRESS.cs
@@ -1,17 +1,4 @@
-﻿// ************************************************* ''
-// DataTools C# Native Utility Library For Windows - Interop
-//
-// Module: IfDefApi
-//         The almighty network interface native API.
-//         Some enum documentation comes from the MSDN.
-//
-// (and an exercise in creative problem solving and data-structure marshaling.)
-//
-// Copyright (C) 2011-2020 Nathan Moschkin
-// All Rights Reserved
-//
-// Licensed Under the Microsoft Public License   
-// ************************************************* ''
+
 
 
 using System;
